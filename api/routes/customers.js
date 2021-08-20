40e56fb7c0ab7245c8134988442fbeb3e86202d0
Ifedayo Sanni <ifedayosanni93@gmail.com>
2021-08-20 16:59:26 +0100

    refactored then/catch to async/await

--- api/routes/customers.js
+++ api/routes/customers.js
@@ -1,33 +1,77 @@
 const express = require("express");
+const { findByIdAndRemove, findByIdAndUpdate, findOneAndUpdate } = require("../../models/customer");
 const router = express.Router();
 const Customer = require("../../models/customer")
 
-router.post("/", (req,res,next)=>{
-    Customer.create(req.body).then(function(customer){
+// router.post("/", (req,res,next)=>{
+//     Customer.create(req.body).then((customer)=>{
+//         res.send(customer)
+//     }).catch(next);     
+// });
+
+router.post("/", async (req,res,next)=>{
+    try{
+        const customer= await Customer.create(req.body)
         res.send(customer)
-    }).catch(next);     
+    }catch(e){
+        console.error(e)
+    };     
 });
 
-router.get("/", (req,res,next)=>{
-    Customer.find({}).then(function(customers){
-        res.send(customers)
-    })
+// router.get("/", (req,res,next)=>{
+//     Customer.find({}).then((customers)=>{
+//         res.send(customers)
+//     })
+// })
+
+router.get("/", async(req,res,next)=>{
+    try{
+        const customers = await Customer.find({});
+        res.status(200).json(customers)
+    }catch(e){
+        console.error(e);
+    }
 })
 
-router.delete("/:id", (req,res,next)=>{
+// router.delete("/:id", (req,res,next)=>{
+//     const id = req.params.id;
+//     Customer.findByIdAndRemove({_id: id}).then((customer)=>{
+//         res.send(customer);
+//     })
+// })
+
+router.delete("/", async(req,res,next)=>{
     const id = req.params.id;
-    Customer.findByIdAndRemove({_id: id}).then(function(customer){
-        res.send(customer);
-    })
+    try{
+        const customer = findByIdAndRemove({_id:id});
+        res.status(200).json(customer)
+    }catch(e){
+        console.error(e)
+    }
 })
 
-router.put("/:id", (req,res,next)=>{
+// router.put("/:id", (req,res,next)=>{
+//     const id = req.params.id;
+//     Customer.findByIdAndUpdate({_id: id},req.body).then(()=>{
+//         Customer.findOneAndUpdate({id}).then((customer)=>{
+//             res.send(customer);
+//             console.log(customer);
+//         });
+//     });
+// })
+
+router.put("/:id", async (req,res,next)=>{
     const id = req.params.id;
-    Customer.findByIdAndUpdate({_id: id},req.body).then(function(){
-        Customer.findOneAndUpdate({id}).then(function(customer){
-            res.send(customer);
-            console.log(customer);
-        });
-    });
+    try {
+        const oldCustomer = await Customer.findByIdAndUpdate({_id:id},req.body);
+
+        if (oldCustomer){
+            const customer = await Customer.findOneAndUpdate(oldCustomer.id);
+            res.status(200).json(customer)
+        }
+    }catch(e){
+        console.error(e)
+    }
 });
+
 module.exports = router;
